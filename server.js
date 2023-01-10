01312083b6178d583fb99e8e0143212af0b296a2
Elai10 <elaiwatson@gmail.com>
2023-01-10 19:13:58 +1000

    login setup

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.post('/users', async (req,res) => {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         const user = {name: req.body.name, password: hashedPassword}
         users.push(user)
+        
         res.status(200).send({
             body:user
         })      
@@ -35,6 +36,7 @@ app.post('/users', async (req,res) => {
 
 app.post('/users/login', async (req, res) => {
     const user = users.find(user => user.name == req.body.name)
+
     if (user == null) {
         return res.status(400).send('Cannot find user')
     }
@@ -42,10 +44,10 @@ app.post('/users/login', async (req, res) => {
        if(await bcrypt.compare(req.body.password, user.password)) {
         res.send('Success')
        } else {
-        res.send('User not found')
+        res.send('User password wrong')
        }
-    } catch {
-        res.status(500).send()
+    } catch(e) {
+        res.status(500).send(e)
     }
 })
 
